4d4df4d134b02d664ffcd35da928ebc30400f11a
qkstarry <qkstarry@163.com>
2022-07-05 14:52:29 +0800

    # v.0.0.6     由修改.jpg改为.png

--- preprocessing/reshape.py
+++ preprocessing/reshape.py
@@ -1,18 +1,13 @@
+#提取目录下所有图片,更改尺寸后保存到另一目录
 from PIL import Image
 import os.path
 import glob
-def convertjpg(jpgfile,outdir,width=128,height=128):
-    img=Image.open(jpgfile)
+def convertjpg(pngfile,outdir,width=128,height=128):
+    img=Image.open(pngfile)
     try:
         new_img=img.resize((width,height),Image.BILINEAR)   
-        new_img.save(os.path.join(outdir,os.path.basename(jpgfile)))
+        new_img.save(os.path.join(outdir,os.path.basename(pngfile)))
     except Exception as e:
         print(e)
-for jpgfile in glob.glob("E:\\img\\*.jpg"): #图片目录
-    convertjpg(jpgfile,"E:\\")        #储存目录
-
-
-'''
-版权声明：本文为CSDN博主「A~朱蒙蒙」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/weixin_43122919/article/details/107465516
-'''
+for pngfile in glob.glob("D:\\test\\*.png"): #图片目录
+    convertjpg(pngfile,"D:\\")        #保持目录
