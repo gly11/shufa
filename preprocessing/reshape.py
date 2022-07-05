c3a7a0ac5ffdb38e70800dada2f7289647c6b9ed
gly11 <gly-11@foxmail.com>
2022-07-05 14:35:28 +0800

    # v0.0.5, updated reshape

--- preprocessing/reshape.py
+++ preprocessing/reshape.py
@@ -10,6 +10,9 @@ def convertjpg(jpgfile,outdir,width=128,height=128):
         print(e)
 for jpgfile in glob.glob("E:\\img\\*.jpg"): #图片目录
     convertjpg(jpgfile,"E:\\")        #储存目录
-————————————————
+
+
+'''
 版权声明：本文为CSDN博主「A~朱蒙蒙」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
 原文链接：https://blog.csdn.net/weixin_43122919/article/details/107465516
+'''
