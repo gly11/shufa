08eaeda6e496bc0d9f1382b6474f7ab971640c9e
gly11 <gly-11@foxmail.com>
2022-07-31 17:36:38 +0800

    # v0.1.6.1, 修改了ResNet_rs的部分错误并重构

--- NeuralNetworks/ResNet_rs/main.py
+++ NeuralNetworks/ResNet_rs/main.py
@@ -1,36 +1,43 @@
 import tensorflow as tf
-import tensorflow.keras.layers as tfl
-import os
+import keras.layers as tfl
+import keras.applications as ka
 
-img_path = ''   #数据路径
-preprocess_input = tf.keras.applications.resnet_rs.preprocess_input
-IMG_SIZE = (160, 160)
-IMG_SHAPE = IMG_SIZE + (3,)
+img_path = ''  # 数据路径
+preprocess_input = ka.resnet_rs.preprocess_input
+IMG_SIZE = (192, 192)
+neuron_numbers = 1
 
-def mymodel(input_shape= IMG_SHAPE ):
-    base_model = tf.keras.applications.resnet_rs.ResNetRS50(input_shape=input_shape,
-                                        include_top=False,
-                                        weights='imagenet')
-    base_model.trainable = False                # freeze the base model by making it non trainable
+
+def mymodel(image_shape=IMG_SIZE):
+    input_shape = image_shape + (3,)
+    base_model = ka.resnet_rs.ResNetRS101(input_shape=input_shape,
+                                          include_top=False,
+                                          weights='imagenet')
+    base_model.trainable = False  # freeze the base model by making it non trainable
     inputs = tf.keras.Input(shape=input_shape)  # 创建输入层
     x = preprocess_input(inputs)
     x = base_model(x, training=False)
+
     # build new layers
     x = tfl.GlobalAveragePooling2D()(x)
     x = tfl.Dropout(0.2)(x)
-
-    outputs = tfl.Dense(1)(x)               # neuron_numbers: hyper-parameter
-
+    outputs = tfl.Dense(neuron_numbers)(x)  # neuron_numbers: hyper-parameter
+    # outputs = x
     model = tf.keras.Model(inputs, outputs)
 
     return model
 
-model2 = mymodel(IMG_SHAPE)
-model2.summary()
 
-# base_learning_rate = 0.001
-# model2.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
-            #   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-            #   metrics=['accuracy'])
-# initial_epochs = 5
-# history = model2.fit(train_dataset, validation_data=validation_dataset, epochs=initial_epochs)
+def main():
+    model2 = mymodel(IMG_SIZE)
+    model2.summary()
+    # base_learning_rate = 0.001
+    # model2.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+    #                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    #                metrics=['accuracy'])
+    # initial_epochs = 5
+    # history = model2.fit(train_dataset, validation_data=validation_dataset, epochs=initial_epochs)
+
+
+if __name__ == '__main__':
+    main()
